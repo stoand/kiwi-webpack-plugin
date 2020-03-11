acde72d89336b952fd794390373f7b76598450b4
Andreas Stocker <andreas@stockers.org>
2020-03-11 23:22:10 +0200

    examples-bank: reference plugin through npm instead of directly

--- examples/bank/webpack.config.js
+++ examples/bank/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const KiwiPlugin = require('../../src/Plugin');
+const KiwiPlugin = require('kiwi-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
