1a3aca6a028949a861ed1ede87e251f9005a6687
Andreas Stocker <andreas@stockers.org>
2020-03-19 01:24:02 +0200

    remove screenshot file and typescript globals definition

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-export const a: any;
-
-declare global {
-  export const describe: (moduleName: string, moduleRun: () => void) => void
-  export const it: (testName: string, testRun: () => void | Promise<any>) => void
-}
