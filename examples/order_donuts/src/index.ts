df22f5b8c95ae6a72898c24dc36b2b6902d88dc8
Andreas Stocker <andreas@stockers.org>
2020-04-21 18:36:49 +0300

    examples-order_donuts: add styles and element demos

--- examples/order_donuts/src/index.ts
+++ examples/order_donuts/src/index.ts
@@ -1,4 +1,25 @@
+require('./style.css');
+
 document.body.innerHTML = 'todo make awesome app';
+
+let value = 0;
+
+let counter = document.createElement('div');
+counter.className = 'blue';
+counter.innerText = value.toString();
+document.body.appendChild(counter);
+
+let button = document.createElement('input');
+button.className = 'red';
+button.type = 'button';
+button.value = 'Asdf';
+
+button.addEventListener('click', () => {
+    value++;
+    counter.innerText = value.toString();
+});
+
+document.body.appendChild(button);
 
 export const asdf = () => {
     let a = 4 
