eab8af3025808de9dacf5048f21af94c3ee2be13
Andreas Stocker <andreas@stockers.org>
2020-05-06 12:20:17 +0300

    actions.list_failed_tests: action works

--- src/actions.ts
+++ src/actions.ts
@@ -1,13 +1,13 @@
 import { RunResult, TestModule, CoveredFiles } from './runner';
 import { init_highlighters, recreateTmpDir, add_location_list_command, line_notifications,
-	line_statuses, running_instances, FileLabels, FileStatuses } from './kakoune_interface';
+        	line_statuses, running_instances, FileLabels, FileStatuses, Location } from './kakoune_interface';
 
 const scanInterval = 350;
 
 let prevScanner: NodeJS.Timeout;
 
 export default function handleTestRun(runResult: RunResult) {
-    
+
     let { modules, initialCoverage } = runResult;
 
     runActions(modules, initialCoverage);
@@ -36,11 +36,11 @@ export default function handleTestRun(runResult: RunResult) {
 
 export function runActions(modules: TestModule[], initialCoverage: CoveredFiles) {
     recreateTmpDir();
-    
+
 	init_highlighters();
 
     setLineStatuses(modules, initialCoverage);
-    
+        
     setNotifications(modules);
 
     addListCommands(modules);
@@ -65,12 +65,12 @@ function setLineStatuses(modules: TestModule[], initialCoverage: CoveredFiles) {
                     } else if (state == 'uncovered' || !state) {
                         state = 'success';
                     }
-				} else {
-    				if (!state) {
-        				state = 'uncovered';
-    				}
-				}
-                
+                } else {
+                    if (!state) {
+                        state = 'uncovered';
+                    }
+                }
+                        
                 fileStatuses[filePath][Number(line) - 1] = state;
             }
         }
@@ -114,6 +114,21 @@ function setNotifications(modules: TestModule[]) {
 }
 
 function addListCommands(modules: TestModule[]) {
+    
+    let failedTests = [];
+
+
+    for (let mod of modules) {
+        for (let test of mod.tests) {
+            if (test.error) {
+                let errorFile = test.error.trace.source;
+                let errorLine = test.error.trace.line;
+                failedTests.push({ file: errorFile, line: errorLine, message: test.error.message });
+            }
+        }
+    }
+
+
     // #SPC-actions.list_failed_tests
-    add_location_list_command('failed_tests', []);
+    add_location_list_command('failed_tests', failedTests);
 }
