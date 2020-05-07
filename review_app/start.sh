d0aa04e842766cbee2c6a597847f93684758fedc
Andreas Stocker <andreas@stockers.org>
2020-05-07 20:09:27 +0300

    review_app: add warning incase py_ts_interfaces package doesn't support current python version

--- review_app/start.sh
+++ review_app/start.sh
@@ -2,6 +2,6 @@
 cd "$(dirname "$0")"
 virtualenv venv --python=python3
 source venv/bin/activate
-pip install -r requirements.txt 
+pip install -r requirements.txt || echo 'Please ensure you have Python >=3.7, <4.0 Installed'
 export FLASK_APP=src/app.py
 flask run --port 8096
