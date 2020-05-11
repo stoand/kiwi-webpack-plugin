9776dab34517dc27e6564b8d3542f93ad2aae6e3
Andreas Stocker <andreas@stockers.org>
2020-05-11 20:16:14 +0300

    actions: move tests into describe and it modules

--- src/actions_tests.ts
+++ src/actions_tests.ts
@@ -1,43 +1,56 @@
 import { computeLocationLists } from './actions';
 import { TestModule, TestResult } from './runner';
 import { expect } from 'chai';
+import { describe, it } from './unit_test_helpers';
 
-let testFile1 = (process.env.HOME || '') + '/t1'; 
-let testFileHomeRelative1 = '~/t1';
+describe('Location List Computation', () => {
 
-let nonTestFile1 = (process.env.HOME || '') + '/f1';
-let nonTestFileHomeRelative1 = '~/f1';
+    let testFile1 = (process.env.HOME || '') + '/t1'; 
+    let testFileHomeRelative1 = '~/t1';
 
-let mockTests: TestResult[] = [
-    { name: 'succ1', trace: { source: testFile1, line: 10, column: 1 }, rawStack: '', stack: [],
-        consoleLogs: [], coveredFiles: { [testFile1]: { 1: true }, [nonTestFile1]: { 15: true } } },
-    { name: 'fail1', trace: { source: '/tmp/b', line: 20, column: 1 }, rawStack: '', stack: [], 
-		error: { rawStack: '', stack: [], message: 'wrong', trace: { source: '/tmp/b', line: 25, column: 1} },
-        consoleLogs: [], coveredFiles: { '/tmp/d': { 35: true } } },
-];
+    let nonTestFile1 = (process.env.HOME || '') + '/f1';
+    let nonTestFileHomeRelative1 = '~/f1';
 
-let mockFileLengths = { [nonTestFile1]: 5, '/tmp/d': 10 };
+    let mockTests: TestResult[] = [
+        { name: 'succ1', trace: { source: testFile1, line: 10, column: 1 }, rawStack: '', stack: [],
+            consoleLogs: [], coveredFiles: { [testFile1]: { 1: true }, [nonTestFile1]: { 15: true } } },
+        { name: 'fail1', trace: { source: '/tmp/b', line: 20, column: 1 }, rawStack: '', stack: [], 
+    		error: { rawStack: '', stack: [], message: 'wrong', trace: { source: '/tmp/b', line: 25, column: 1} },
+            consoleLogs: [], coveredFiles: { '/tmp/d': { 35: true } } },
+    ];
 
-let mockModules: TestModule[] = [{ name: 'mod1', tests: mockTests }];
+    let mockFileLengths = { [nonTestFile1]: 5, '/tmp/d': 10 };
 
-let { failed_tests, all_tests, all_test_files, all_covered_non_test_files } = computeLocationLists(mockModules, mockFileLengths);
+    let mockModules: TestModule[] = [{ name: 'mod1', tests: mockTests }];
 
-// #SPC-actions.tst-list_failed_tests
-expect(failed_tests).to.eql([{ file: '/tmp/b', line: 25, message: 'fail1 - wrong' }]);
+    let { failed_tests, all_tests, all_test_files, all_covered_non_test_files } = computeLocationLists(mockModules, mockFileLengths);
 
-// #SPC-actions.tst-list_all_tests
-expect(all_tests).to.eql([
-    { file: testFileHomeRelative1, line: 10, message: 'succ1' },
-    { file: '/tmp/b', line: 20, message: 'fail1' }]);
-        
-// #SPC-actions.tst-list_all_test_files
-expect(all_test_files).to.eql([
-    // jump to the line number of the first failing test - if there is any
-    // also sort the files by those that have the most failing tests first
-    { file: '/tmp/b', line: 25, message: '0/1 - wrong', pass: 0, fail: 1 },
-    { file: testFileHomeRelative1, line: 1, message: '1/0', pass: 1, fail: 0 } ]);
-        
-// #SPC-actions.tst-list_all_covered_non_test_files
-expect(all_covered_non_test_files).to.eql([
-    { file: '/tmp/d', line: 35, message: '[10%] 0/1 - wrong', pass: 0, fail: 1, coveragePercent: 10 },
-    { file: nonTestFileHomeRelative1, line: 1, message: '[20%] 1/0', pass: 1, fail: 0, coveragePercent: 20 }]);
+    it('can compute failed test list', () => {
+        // #SPC-actions.tst-list_failed_tests
+        expect(failed_tests).to.eql([{ file: '/tmp/b', line: 25, message: 'fail1 - wrong' }]);
+    });
+
+
+    it('can compute a list of all tests', () => {
+        // #SPC-actions.tst-list_all_tests
+        expect(all_tests).to.eql([
+            { file: testFileHomeRelative1, line: 10, message: 'succ1' },
+            { file: '/tmp/b', line: 20, message: 'fail1' }]);
+    });
+
+    it('can compute a list of all test files', () => {
+        // #SPC-actions.tst-list_all_test_files
+        expect(all_test_files).to.eql([
+            // jump to the line number of the first failing test - if there is any
+            // also sort the files by those that have the most failing tests first
+            { file: '/tmp/b', line: 25, message: '0/1 - wrong', pass: 0, fail: 1 },
+            { file: testFileHomeRelative1, line: 1, message: '1/0', pass: 1, fail: 0 } ]);
+    });
+
+    it('can compute a list of non-test files', () => {
+        // #SPC-actions.tst-list_all_covered_non_test_files
+        expect(all_covered_non_test_files).to.eql([
+            { file: '/tmp/d', line: 35, message: '[10%] 0/1 - wrong', pass: 0, fail: 1, coveragePercent: 10 },
+            { file: nonTestFileHomeRelative1, line: 1, message: '[20%] 1/0', pass: 1, fail: 0, coveragePercent: 20 }]);
+    });
+});
---
+++ src/unit_test_helpers.ts
@@ -0,0 +1,3 @@
+export function describe(_name: string, fn: Function) { fn(); }
+
+export function it(_description: string, fn: Function) { fn(); }
