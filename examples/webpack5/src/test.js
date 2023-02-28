aee9b64269a7aad49be9d27820fe626dabb0cc47
Andreas Stocker <shadowstep7@gmail.com>
2023-02-28 18:05:36 +0100

    webpack5: add test and kiwi plugin

---
+++ examples/webpack5/src/test.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('tests', () => {
+    it('works', () => {
+        expect('asdf').to.equal('asdf');
+    });
+});
--- examples/webpack5/webpack.config.js
+++ examples/webpack5/webpack.config.js
@@ -1,5 +1,14 @@
+const KiwiPlugin = require('kiwi-webpack-plugin');
+
 module.exports = {
     entry: './src/app.js',
     devtool: 'source-map',
     target: 'node',
+    plugins: [
+        new KiwiPlugin({
+            testEntry: './src/test.js',
+            stopBuildOnFail: false,
+            runner: 'node',
+        }),
+    ],
 };
